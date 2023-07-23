5c50c49bb141d34d512a6cd154ec03bc37dae593
Harry Phan <117452450+nghiaphantien1090@users.noreply.github.com>
2023-07-23 22:34:36 +0700

    Update style.ts

--- src/style.ts
+++ src/style.ts
@@ -1,30 +1,30 @@
 export const backgroundImage = [
-`md:bg-[url('./src/assets/image/bg-hero-1-900-x1.png')]
- lg:bg-[url('./src/assets/image/bg-hero-1-1792-x1.png')] 
+`md:bg-[url('/src/assets/image/bg-hero-1-900-x1.png')]
+ lg:bg-[url('/src/assets/image/bg-hero-1-1792-x1.png')] 
  md:bg-[#023a15]
 ` 
-,`md:bg-[url('./src/assets/image/bg-hero-2-900-x1.png')] 
- lg:bg-[url('./src/assets/image/bg-hero-2-1792-x1.png')] 
- md:bg-[#b64762] ok
+,`md:bg-[url('/src/assets/image/bg-hero-2-900-x1.png')] 
+ lg:bg-[url('/src/assets/image/bg-hero-2-1792-x1.png')] 
+ md:bg-[#b64762] 
 ` 
-,`md:bg-[url('./src/assets/image/bg-hero-3-900-x1.png')] 
- lg:bg-[url('./src/assets/image/bg-hero-3-1792-x1.png')] 
- md:bg-[#540e1f] ok
+,`md:bg-[url('/src/assets/image/bg-hero-3-900-x1.png')] 
+ lg:bg-[url('/src/assets/image/bg-hero-3-1792-x1.png')] 
+ md:bg-[#540e1f] 
 ` 
-,`md:bg-[url('./src/assets/image/bg-hero-4-900-x1.png')] 
- lg:bg-[url('./src/assets/image/bg-hero-4-1792-x1.png')] 
- md:bg-[#023a15] ok
+,`md:bg-[url('/src/assets/image/bg-hero-4-900-x1.png')] 
+ lg:bg-[url('/src/assets/image/bg-hero-4-1792-x1.png')] 
+ md:bg-[#023a15] 
 ` 
-,`md:bg-[url('./src/assets/image/bg-hero-5-900-x1.png')] 
- lg:bg-[url('./src/assets/image/bg-hero-5-1792-x1.png')] 
+,`md:bg-[url('/src/assets/image/bg-hero-5-900-x1.png')] 
+ lg:bg-[url('/src/assets/image/bg-hero-5-1792-x1.png')] 
  md:bg-[#7d1a00]
 ` 
-,`md:bg-[url('./src/assets/image/bg-hero-6-900-x1.png')] 
- lg:bg-[url('./src/assets/image/bg-hero-6-1792-x1.png')] 
- md:bg-[#1a1b1d] ok
+,`md:bg-[url('/src/assets/image/bg-hero-6-900-x1.png')] 
+ lg:bg-[url('/src/assets/image/bg-hero-6-1792-x1.png')] 
+ md:bg-[#1a1b1d] 
 ` 
 ]
-export const fiveStar= [`md:bg-[url('./src/assets/image/fiveStarRating.svg')] 
+export const fiveStar= [`md:bg-[url('/src/assets/image/fiveStarRating.svg')] 
 ` ]
-export const fiveStar2= [`md:before:bg-[url('./src/assets/image/fiveStarRating.svg')] 
+export const fiveStar2= [`md:before:bg-[url('/src/assets/image/fiveStarRating.svg')] 
 ` ]
